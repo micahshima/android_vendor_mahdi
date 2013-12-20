SlimRoms/vendor_slim

[1/2] LCD density backup
Change-Id: I2b8c7aa2419b689d5bb98a02e3c68dd2e5e0d527

config/common.mk
@@ -14,7 +14,9 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_COPY_FILES += \
     vendor/slim/prebuilt/common/bin/backuptool.sh:system/bin/backuptool.sh \
     vendor/slim/prebuilt/common/bin/backuptool.functions:system/bin/backuptool.functions \
-    vendor/slim/prebuilt/common/bin/50-slim.sh:system/addon.d/50-slim.sh
+    vendor/slim/prebuilt/common/bin/50-slim.sh:system/addon.d/50-slim.sh \
+    vendor/slim/prebuilt/common/bin/99-backup.sh:system/addon.d/99-backup.sh \
+    vendor/slim/prebuilt/common/etc/backup.conf:system/etc/backup.conf
 
 # SLIM-specific init file
 PRODUCT_COPY_FILES += \

micahshima/android_vendor_mahdi

Conflicts:
	config/common.mk

configs/common.mk
@@ -49,7 +49,9 @@ PRODUCT_COPY_FILES += \
     vendor/mahdi/prebuilt/common/bin/backuptool.sh:system/bin/backuptool.sh \
     vendor/mahdi/prebuilt/common/bin/backuptool.functions:system/bin/backuptool.functions \
     vendor/mahdi/prebuilt/common/bin/blacklist:system/addon.d/blacklist \
-    vendor/mahdi/prebuilt/common/bin/50-mahdi.sh:system/addon.d/50-mahdi.sh
+    vendor/mahdi/prebuilt/common/bin/50-mahdi.sh:system/addon.d/50-mahdi.sh \
+    vendor/mahdi/prebuilt/common/bin/99-backup.sh:system/addon.d/99-backup.sh \
+    vendor/mahdi/prebuilt/common/etc/backup.conf:system/etc/backup.conf
 
 # Superuser
 SUPERUSER_EMBEDDED := false